466bde4470f1b47a1efdb7cb1777ecce462df825
Lynn Taylor <ltaylor3@doc.gov>
2011-04-07 11:20:53 -0400

    first commit of file dev-to-testing.php

---
+++ dev-to-testing.php
@@ -0,0 +1,10 @@
+<?php
+echo "Running automated tests.\n";
+echo "Tests completed successfully.\n";
+echo "Committing files to central repository.\n";
+echo "Files successfully committed to central repository.\n";
+echo "Moving files to varEnvironment.\n";
+echo "Files successfully moved to varEnvironment.\n";
+echo "Sending out notifications by email.\n";
+echo "Sitename is ready for testing in varEnvironment. The following changes have been made:\n";
+?>
